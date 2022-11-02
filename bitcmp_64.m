f9f366461343d5b22c81516e8c235435442bec9d
Paul <95068443+paulchen2713@users.noreply.github.com>
2022-11-02 12:42:23 +0800

    Update bitcmp_64.m

--- bitcmp_64.m
+++ bitcmp_64.m
@@ -2,26 +2,26 @@
 % bitcmp for 64bit
 %
 function out = bitcmp_64(a_hex)
-a = a_hex;
-%
-% split a into left, and right parts in char string type
-a_left = a(1:8);
-a_right = a(9:16);
-%
-% convert a into decimal type
-a_left_dec = uint32(hex2dec(a_left));
-a_right_dec = uint32(hex2dec(a_right));
-%
-% executes bit-wise complement opertion
-a_left_dec = bitcmp(a_left_dec);
-a_right_dec = bitcmp(a_right_dec);
-%
-% turn a back into decimal type
-a_left_hex = dec2hex(a_left_dec, 8);
-a_right_hex = dec2hex(a_right_dec, 8);
-%
-% concatenate left and right string
-a_hex = strcat(a_left_hex, a_right_hex);
-out = a_hex;
+    a = a_hex;
+    %
+    % split a into left, and right parts in char string type
+    a_left = a(1:8);
+    a_right = a(9:16);
+    %
+    % convert a into decimal type
+    a_left_dec = uint32(hex2dec(a_left));
+    a_right_dec = uint32(hex2dec(a_right));
+    %
+    % executes bit-wise complement opertion
+    a_left_dec = bitcmp(a_left_dec);
+    a_right_dec = bitcmp(a_right_dec);
+    %
+    % turn a back into decimal type
+    a_left_hex = dec2hex(a_left_dec, 8);
+    a_right_hex = dec2hex(a_right_dec, 8);
+    %
+    % concatenate left and right string
+    a_hex = strcat(a_left_hex, a_right_hex);
+    out = a_hex;
 return
 
